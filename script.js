09ab11e1e571651a7ac9a518ed3175227677f859
Dominik <Dominik@DominikUni.hfu-open.fu.hs-furtwangen.de>
2017-03-23 16:16:47 +0100

    location api

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     function vibrate() {
         navigator.vibrate(500);
     }
-    document.getElementById("wrapper").addEventListener("click", vibrate);
+    function giveLocation() {
+        var url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCvET66xGzrZGKTGrx9nQIH-Y7T2nnwxRk";
+        var give = $.ajax({ type: "POST",
+            url: url,
+            dataType: "json",
+            success: function (data) {
+                console.log(give.responseText);
+                var obj = JSON.parse(give.responseText);
+                console.log(obj.location.lat);
+                document.getElementById("wrapper").innerHTML = obj.accuracy;
+            }
+        });
+    }
+    document.getElementById("wrapper").addEventListener("click", giveLocation);
 });
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -4,6 +4,28 @@ document.addEventListener('DOMContentLoaded', function() {
         navigator.vibrate(500);
     }
 
-    document.getElementById("wrapper").addEventListener("click", vibrate);
+    function giveLocation():void {
+        var url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCvET66xGzrZGKTGrx9nQIH-Y7T2nnwxRk";
+        
+        var give = $.ajax(
+{           type : "POST",
+            url : url,
+            dataType: "json",
+            
+            success: function(data){
+                console.log(give.responseText);
+                var obj = JSON.parse(give.responseText);
+                console.log(obj.location.lat)
+                document.getElementById("wrapper").innerHTML=obj.accuracy;
+                
+                }
 
-});
+});  
+        
+   
+        
+    }
+        document.getElementById("wrapper").addEventListener("click", giveLocation);
+
+    
+    });
