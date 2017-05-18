b31db621c4402ac7ee6d81fb7afa8e98b04ee95d
Dominik <Dominik@DominikUni.hfu-open.fu.hs-furtwangen.de>
2017-05-18 14:27:55 +0200

    asynchronous -.-

--- script2.js
+++ script2.js
@@ -1,17 +1,24 @@
 var places;
 (function (places) {
     var x = document.getElementById("ausgabe");
-    document.addEventListener('DOMContentLoaded', init);
+    document.addEventListener("DOMContentLoaded", init);
     var poi = ["Robert-Gerwig-Platz 1, 78120 Furtwangen im Schwarzwald", "Friedrichstrasse 17, 78120 Furtwangen im Schwarzwald", "Marktpl. 9, 78120 Furtwangen im Schwarzwald", "Colnestrasse 6, 78120 Furtwangen im Schwarzwald"];
     var oldRange = 99999999999999999;
+    var bla;
+    var latyay;
+    var lngyay;
     function init() {
-        document.getElementById("wrapper").addEventListener("click", getLocation);
+        document.getElementById("wrapper").addEventListener("click", geocodeIt);
     }
     function vibrate() {
         navigator.vibrate(500);
         console.log("vibrate");
     }
-    function getLocation() {
+    function getLocation(lat, lng) {
+        lat = lat;
+        lng = lng;
+        console.log(latyay);
+        console.log(lngyay);
         if (navigator.geolocation) {
             console.log("showPos");
             navigator.geolocation.watchPosition(calcDistance);
@@ -22,19 +29,17 @@ var places;
     }
     function calcDistance(position) {
         var p1 = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        var p2 = poi[0];
+        var p2 = new google.maps.LatLng(lat, lng);
         document.getElementById("ausgabe").innerHTML = (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
     }
     function geocodeIt() {
-        var toGC = poi[0];
         var geocoder = new google.maps.Geocoder();
-        service.getDistanceMatrix({
-            origins: [origin1],
-            destinations: [poi[x]],
-            travelMode: google.maps.TravelMode.DRIVING,
-        }, callback);
-        function callback(response, status) {
-        }
+        var address = "new york";
+        geocoder.geocode({ "address": address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                getLocation(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+            }
+        });
     }
 })(places || (places = {}));
 //# sourceMappingURL=script2.js.map
--- script2.ts
+++ script2.ts
@@ -1,47 +1,51 @@
 namespace places {
-var x: HTMLElement = document.getElementById("ausgabe");
-document.addEventListener('DOMContentLoaded', init);
-var poi: String[] = ["Robert-Gerwig-Platz 1, 78120 Furtwangen im Schwarzwald", "Friedrichstrasse 17, 78120 Furtwangen im Schwarzwald", "Marktpl. 9, 78120 Furtwangen im Schwarzwald", "Colnestrasse 6, 78120 Furtwangen im Schwarzwald"];
-var oldRange: number = 99999999999999999;
+    var x: HTMLElement = document.getElementById("ausgabe");
+    document.addEventListener("DOMContentLoaded", init);
+    var poi: string[] = ["Robert-Gerwig-Platz 1, 78120 Furtwangen im Schwarzwald", "Friedrichstrasse 17, 78120 Furtwangen im Schwarzwald", "Marktpl. 9, 78120 Furtwangen im Schwarzwald", "Colnestrasse 6, 78120 Furtwangen im Schwarzwald"];
+    var oldRange: number = 99999999999999999;
+    var bla: any[];
+    var latyay: any;
+    var lngyay: any;
 
-function init(): void {
+    function init(): void {
 
-    document.getElementById("wrapper").addEventListener("click", getLocation);
-}
-
-
-function vibrate(): void {
-    navigator.vibrate(500);
-    console.log("vibrate")
-}
-
-function getLocation(): void {
-    if (navigator.geolocation) {
-        console.log("showPos");
-        navigator.geolocation.watchPosition(calcDistance);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        document.getElementById("wrapper").addEventListener("click", geocodeIt);
     }
-}
 
-function calcDistance(position): void {
-    var p1 = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var p2 =  poi[0];
-    document.getElementById("ausgabe").innerHTML = (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
-}
-    
 
-function geocodeIt(): void {
-    var toGC = poi[0];
-    var geocoder = new google.maps.Geocoder();
-        service.getDistanceMatrix(
-            {
-                origins: [origin1],
-                destinations: [poi[x]],
-                travelMode: google.maps.TravelMode.DRIVING,
-            }, callback);
+    function vibrate(): void {
+        navigator.vibrate(500);
+        console.log("vibrate");
+    }
 
-        function callback(response, status) {
+    function getLocation(lat: any, lng: any): void {
+        lat = lat;
+        lng = lng;
+        console.log(latyay);
+        console.log(lngyay);
+        if (navigator.geolocation) {
+            console.log("showPos");
+            navigator.geolocation.watchPosition(calcDistance);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
         }
+    }
+
+    function calcDistance(position): void {
+        var p1: any = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        var p2: any = new google.maps.LatLng(lat, lng);
+        document.getElementById("ausgabe").innerHTML = (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
+    }
+
+    function geocodeIt() {
+        var geocoder = new google.maps.Geocoder();
+        var address = "new york";
+
+        geocoder.geocode({ "address": address }, function(results, status) {
+
+            if (status == google.maps.GeocoderStatus.OK) {
+                getLocation(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+            }
+        });
     }
 }
